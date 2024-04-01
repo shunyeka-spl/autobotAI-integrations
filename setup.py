dea3a17a9a1e7f2fdb52eafaf327b1da3d44d768
Vishal Shandilya <vishal.shandilya@shunyeka.com>
2024-04-01 17:58:22 +0530

    Adds package_data to setup.py for yml & json files

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ setup(
     author_email='hello@shunyeka.com',
     description='A python package that contains all the integrations for autobotAI',
     packages=find_packages(),
+    package_data={'': ['integrations/*/inventory.json', 'integrations/*/python_sdk_clients.yml']},
     classifiers=[
         'License :: Other/Proprietary License'
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.11'
     ],
     python_requires='>=3.11',
+    include_package_data=True
+
 )

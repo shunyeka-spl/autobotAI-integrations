cd9c06475ea3a7b68ff2db184684663da05f2a55
Vishal Shandilya <vishal.shandilya@shunyeka.com>
2024-04-15 03:36:56 +0530

    Minor Fixes to Payload Schema

--- task_executor.py
+++ task_executor.py
@@ -6,19 +6,20 @@ from autobotAI_integrations.integrations import integration_service_factory
 from autobotAI_integrations.models import ConnectionInterfaces
 from autobotAI_integrations.payload_schema import TaskResult, PayloadTask
 
+
 class TaskExecutor:
     def __init__(self, dir_path=os.getcwd()) -> None:
         self.task_results = dict()
         self.input_dir = dir_path
-    
+
     def get_task_result_by_id(self, task_id):
         if self.task_results.get(task_id, None):
             return self.task_results[task_id]
         raise KeyError("TaskID {} Not Found!".format(task_id))
-    
+
     def get_all_results(self):
         return self.task_results
-    
+
     def get_task(self, task_id):
         with open(os.path.join(self.input_dir, f"{task_id}.json"), 'r') as task_file:
             task = PayloadTask.model_validate_json(task_file.read(), strict=False)
@@ -29,7 +30,7 @@ class TaskExecutor:
         with open(task_output_file, "w") as task_file:
             task_result = self.get_task_result_by_id(task_id=task_id)
             task_file.write(json.dumps(task_result, default=str))
-        
+
     def run(self, task_id):
         task = self.get_task(task_id)
         integration = IntegrationSchema.model_validate(task.context.integration)

42fccedd9bd641b98c14943711038e5789d1cd27
Amit Chotaliya <a@Amits-MacBook-Air.local>
2024-04-17 14:04:40 +0530

    Made a few changes related to integration details

--- autobotAI_integrations/integrations/aws/__init__.py
+++ autobotAI_integrations/integrations/aws/__init__.py
@@ -101,12 +101,11 @@ class AWSService(BaseService):
     @classmethod
     def get_details(cls):
         return {
-            "automation_code": "",
-            "fetcher_code": "",
             "clients": list_of_unique_elements(cls.get_all_python_sdk_clients()),
             "supported_executor": "ecs",
             "compliance_supported": False,
-        "supported_interfaces": cls.supported_connection_interfaces()
+            "supported_interfaces": cls.supported_connection_interfaces(),
+            "python_code_sample": "print('hello world')"
     }
 
     def generate_steampipe_creds(self) -> SteampipeCreds:

e99c9da21d1fbd06a440756dae45dbeef9244a7d
Tarique B <86839948+Tarique-B-DevOps@users.noreply.github.com>
2024-04-22 16:53:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     install_requires=[
         "boto3",
         "botocore",
+        "python-dotenv",
+        "azure-identity",
         "pydantic",
         "PyYAML",
         "pydash",

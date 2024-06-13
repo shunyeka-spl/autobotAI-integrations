4357a6292648148da7bdf2a2cf2139b9429d7163
Vishal Shandilya <vishal.shandilya@shunyeka.com>
2024-06-13 19:38:32 +0530

    First Release - Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autobotAI_integrations",
-    version="0.0.0a",
+    version="1.0.0",
     author="ShunyEka Systems Private Limited",
     author_email="hello@shunyeka.com",
     description="A python package that contains all the integrations for autobotAI",

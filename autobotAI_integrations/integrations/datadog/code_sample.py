83f401faa372ee00f97ff276194cb4d4ff0971c5
Ritin Tiwari <ritintiwari417@gmail.com>
2024-07-10 13:26:55 +0530

    Added Datadog integration (#58)

---
+++ autobotAI_integrations/integrations/datadog/code_sample.py
@@ -0,0 +1,33 @@
+# Import your modules here
+import json
+
+# **Security Note:** Client-related modules should not be directly imported here.
+# Instead, they are passed as arguments and retrieved from a secure configuration.
+
+
+def executor(context):
+    """
+    Executes provided Python code within integrations.
+
+    Args:
+        context (dict): A dictionary containing information about the current execution.
+            - params (dict): A dictionary containing parameters specified while creating action.
+            - clients (dict): A dictionary that contain selected client objects while defining action. (The specific clients present and their usage depend on the specific action being executed.)
+
+    Returns:
+        list: Always returns an empty list (`[]`) or a list containing the results of the code execution. The specific content of the returned list depends on the code and how it interacts with the integration.
+    """
+
+    params = context["params"]
+    clients = context["clients"]
+
+    # Placeholder for retrieving the integration-specific client if needed
+    client = clients["datadog_api_client"]  # Supports only one client
+    # To Learn more about usage visit: https://datadogpy.readthedocs.io/en/latest/
+
+    # User's Python code execution logic goes here
+    # (Replace this comment with the your actual code)
+
+    # Example: Get the total number of active host scrapes(for illustration purposes only)
+    result = client.Hosts.totals()
+    return result  # Replace with your actual return logic

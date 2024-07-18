4f1e750120621d1e666fc1c6cdaac97f1d6673e8
Ritin Tiwari <ritintiwari417@gmail.com>
2024-07-18 14:10:36 +0530

    Splunk integration (#62)
    * Added newrelic test
    
    * newrelic integration commited
    
    * Fixed newrelic is not available error
    
    * Added steampipe tables and fixed test cases
    
    * api_key issue fixed
    
    * Fixed test
    
    * optimized newrelic api
    
    * splunk integration added
    
    * Revert "splunk integration added"
    
    This reverts commit 49549856d84f9c226384ac64a9fc7d5f12fdfb26.
    
    * files added
    
    ---------
    
    Co-authored-by: sree <sree@gmail.com>

---
+++ autobotAI_integrations/integrations/newrelic/__init__.py
@@ -0,0 +1,109 @@
+from typing import List, Type, Union
+
+from autobotAI_integrations.models import *
+from autobotAI_integrations import (
+    BaseSchema,
+    BaseService,
+    ConnectionInterfaces,
+)
+import requests
+
+
+class NewrelicIntegrations(BaseSchema):
+    api_key: Optional[str] = Field(default=None, exclude=True)
+    region: Optional[str] = Field(default="us", exclude=False)
+
+    category: Optional[str] = IntegrationCategory.MONITORING_TOOLS.value
+    description: Optional[str] = (
+        "New Relic is a SaaS providing Monitoring, Alerting, Dashboards for applications, infrastructure, etc."
+    )
+
+
+class NewrelicService(BaseService):
+
+    def __init__(self, ctx: dict, integration: Union[NewrelicIntegrations, dict]):
+        """
+        Integration should have all the data regarding the integration
+        """
+        if not isinstance(integration, NewrelicIntegrations):
+            integration = NewrelicIntegrations(**integration)
+        super().__init__(ctx, integration)
+
+    def _test_integration(self) -> dict:
+        try:
+            response = requests.get(
+                "https://api.newrelic.com/v2/applications.json",
+                headers={"X-Api-Key": self.integration.api_key},
+            )
+
+            if response.status_code == 200:
+                return {"success": True}
+            else:
+                return {
+                "success": False,
+                "error": f"API request failed. Status code: {response.status_code}",
+            }
+        except requests.exceptions.ConnectionError as e:
+            return {"success": False, "error": "Connection is unreachable"}
+
+    @staticmethod
+    def get_forms():
+        return {
+            "label": "Newrelic",
+            "type": "form",
+            "children": [
+                {
+                    "name": "api_key",
+                    "type": "text/password",
+                    "label": "API Key",
+                    "placeholder": "Enter the Newrelic API Key",
+                    "required": True,
+                },
+                {
+                    "name": "region",
+                    "type": "text",
+                    "label": "Region",
+                    "placeholder": "'us' or 'eu',default:'us'",
+                    "required": True,
+                },
+            ],
+        }
+
+    @staticmethod
+    def get_schema() -> Type[BaseSchema]:
+        return NewrelicIntegrations
+
+    @classmethod
+    def get_details(cls):
+        return {
+            "clients": [],
+            "supported_executor": "ecs",
+            "compliance_supported": False,
+            "supported_interfaces": cls.supported_connection_interfaces(),
+        }
+
+    @staticmethod
+    def supported_connection_interfaces():
+        return [
+            ConnectionInterfaces.STEAMPIPE,
+            ConnectionInterfaces.REST_API,
+            ConnectionInterfaces.CLI,
+        ]
+
+    def generate_steampipe_creds(self) -> SteampipeCreds:
+        creds = {
+            "NEW_RELIC_API_KEY": self.integration.api_key,
+            "NEW_RELIC_REGION": self.integration.region,
+        }
+        conf_path = "~/.steampipe/config/newrelic.spc"
+        config = """connection "newrelic" {
+  plugin = "newrelic"
+}
+"""
+        return SteampipeCreds(
+            envs=creds,
+            plugin_name="newrelic",
+            connection_name="newrelic",
+            conf_path=conf_path,
+            config=config,
+        )
---
+++ tests/test_integrations/test_splunk.py
@@ -0,0 +1,71 @@
+
+import pytest
+
+from autobotAI_integrations.handlers.task_handler import handle_task
+from autobotAI_integrations.integrations import integration_service_factory
+
+# splunk_python_code = """
+# # Import your modules here
+# import json
+
+# # **Security Note:** Client-related modules should not be directly imported here.
+# # Instead, they are passed as arguments and retrieved from a secure configuration.
+
+
+# def executor(context):
+#     params = context["params"]
+#     clients = context["clients"]
+#     try:
+#         # Placeholder for retrieving the integration-specific client if needed
+#         client = clients["Splunk"]  # Supports only one client
+
+#         # Example: Code to search for events (for illustration purposes only)
+#         search_query = "search index=main | head 10"
+#         result = client.search(search_query)
+#         return [result] # Replace with your actual return logic
+#     except Exception as e: 
+#         return {
+#             "error": e,
+#             "clients": context["clients"]
+#         }
+# """
+
+class TestClassSplunk:
+    def test_splunk_steampipe_task(self, get_keys, sample_integration_dict, sample_steampipe_task, test_result_format):
+        tokens = {
+            "username": get_keys["SPLUNK_USERNAME"],
+            "password": get_keys["SPLUNK_PASSWORD"]
+        }
+        integration = sample_integration_dict("splunk", tokens)
+        splunk_query = "select * from splunk_search"
+        task = sample_steampipe_task(integration, query=splunk_query)
+        result = handle_task(task)
+        test_result_format(result)
+
+    # def test_splunk_python_task(self, get_keys, sample_integration_dict, sample_python_task, test_result_format):
+    #     tokens = {
+    #         "username": get_keys["SPLUNK_USERNAME"],
+    #         "password": get_keys["SPLUNK_PASSWORD"]
+    #     }
+    #     integration = sample_integration_dict("splunk", tokens)
+    #     task = sample_python_task(integration, code=splunk_python_code, clients=["Splunk"])
+    #     result = handle_task(task)
+    #     test_result_format(result)
+
+    def test_integration_active(self, get_keys, sample_integration_dict):
+        tokens = {
+            "username": get_keys["SPLUNK_USERNAME"],
+            "password": get_keys["SPLUNK_PASSWORD"]
+        }
+        integration = sample_integration_dict("splunk", tokens)
+        service = integration_service_factory.get_service(None, integration)
+        res = service.is_active()
+        assert res["success"]
+        tokens = {
+            "username": get_keys["SPLUNK_USERNAME"],
+            "password": "invalid_password"
+        }
+        integration = sample_integration_dict("splunk", tokens)
+        service = integration_service_factory.get_service(None, integration)
+        res = service.is_active()
+        assert not res["success"]

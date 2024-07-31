9daf00261d8a4081b1ddb3142f90a119a765f1fd
Ritin Tiwari <ritintiwari417@gmail.com>
2024-07-31 13:02:54 +0530

    AbuseIpdb optimization

--- autobotAI_integrations/integrations/abuseipdb/__init__.py
+++ autobotAI_integrations/integrations/abuseipdb/__init__.py
@@ -1,12 +1,8 @@
-import uuid
-from typing import List, Type, Union
+from typing import Type, Union
 
-from autobotAI_integrations import list_of_unique_elements
 from autobotAI_integrations.models import *
-from autobotAI_integrations.models import List
 from autobotAI_integrations import (
     BaseSchema,
-    CLICreds,
     BaseService,
     ConnectionInterfaces,
 )
@@ -16,6 +12,7 @@ import requests
 class AbuseIPDBIntegration(BaseSchema):
     api_key: Optional[str] = Field(default=None, exclude=True)
 
+    name: str = "AbuseIPDB"
     category: Optional[str] = IntegrationCategory.SECURITY_TOOLS.value
     description: Optional[str] = (
         "AbuseIPDB is a project dedicated to helping combat the spread of hackers, spammers, and abusive activity on the internet."
@@ -33,10 +30,11 @@ class AbuseIPDBService(BaseService):
         super().__init__(ctx, integration)
 
     def _test_integration(self) -> dict:
-        url = "https://api.abuseipdb.com/api/v2/check"
-        params = {"ipAddress": "118.25.6.39", "maxAgeInDays": 90, "verbose": True}
-        headers = {"Key": self.integration.api_key, "Accept": "application/json"}
-        response = requests.get(url, params=params, headers=headers)
+        response = requests.get(
+            "https://api.abuseipdb.com/api/v2/check",
+            params={"ipAddress": "118.25.6.39", "maxAgeInDays": 90, "verbose": True},
+            headers={"Key": self.integration.api_key, "Accept": "application/json"},
+        )
         if response.status_code == 200:
             return {"success": True}
         else:
@@ -64,15 +62,6 @@ class AbuseIPDBService(BaseService):
     @staticmethod
     def get_schema() -> Type[BaseSchema]:
         return AbuseIPDBIntegration
-
-    @classmethod
-    def get_details(cls):
-        return {
-            "clients": [],
-            "supported_executor": "ecs",
-            "compliance_supported": False,
-            "supported_interfaces": cls.supported_connection_interfaces(),
-        }
 
     @staticmethod
     def supported_connection_interfaces():

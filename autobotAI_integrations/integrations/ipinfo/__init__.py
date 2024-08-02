b0e886167cdeadbcfba91605a2931e95fb17f6f4
Sreekanth7Gs <96763290+Sreekanth7Gs@users.noreply.github.com>
2024-08-02 12:01:34 +0530

    Ipstack and ipinfo (#71)
    * ipstackand and ipinfo integration files added
    
    * __init__ file modified
    
    * ipinfo integration fix
    
    * Ipstack integration fixes
    
    ---------
    
    Co-authored-by: sree <sree@gmail.com>
    Co-authored-by: Ritin Tiwari <ritintiwari417@gmail.com>

---
+++ autobotAI_integrations/integrations/ipinfo/__init__.py
@@ -0,0 +1,86 @@
+from typing import List, Type, Union, Optional
+
+from autobotAI_integrations.models import *
+from autobotAI_integrations import (
+    BaseSchema,
+    BaseService,
+    ConnectionInterfaces,
+)
+import requests
+
+
+class IPinfoIntegrations(BaseSchema):
+    token: Optional[str] = Field(default=None, exclude=True)
+
+    name: Optional[str] = "IPInfo"
+    category: Optional[str] = IntegrationCategory.SECURITY_TOOLS.value
+    description: Optional[str] = (
+        "IPInfo.io is an API for IP address information (e.g. location)."
+    )
+
+
+class IPinfoService(BaseService):
+
+    def __init__(self, ctx: dict, integration: Union[IPinfoIntegrations, dict]):
+        """
+        Integration should have all the data regarding the integration
+        """
+        if not isinstance(integration, IPinfoIntegrations):
+            integration = IPinfoIntegrations(**integration)
+        super().__init__(ctx, integration)
+
+    def _test_integration(self) -> dict:
+        try:
+            response = requests.get("https://ipinfo.io/8.8.8.8/json", params={
+                "token": self.integration.token
+            })
+            if response.status_code == 200:
+                return {"success": True}
+            else:
+                return {
+                    "success": False, "error": f"Request failed with status code: {response.status_code}"
+                }
+        except Exception as e:
+            return {
+                "success": False, "error": f"Request failed with error: {str(e)}"
+            }
+
+    @staticmethod
+    def get_forms():
+        return {
+            "label": "IPInfo",
+            "type": "form",
+            "children": [
+                {
+                    "label": "API Key",
+                    "type": "text",
+                    "name": "token",
+                    "required": False,
+                }
+            ],
+        }
+
+    @staticmethod
+    def get_schema() -> Type[BaseSchema]:
+        return IPinfoIntegrations
+
+    @staticmethod
+    def supported_connection_interfaces():
+        return [
+            ConnectionInterfaces.REST_API,
+            ConnectionInterfaces.STEAMPIPE
+        ]
+
+    def generate_steampipe_creds(self) -> SteampipeCreds:
+        conf_path = "~/.steampipe/config/ipinfo.spc"
+        config = """connection "ipinfo" {
+  plugin = "ipinfo"
+}
+"""
+        return SteampipeCreds(
+            envs={"IPINFO_TOKEN": self.integration.token},
+            plugin_name="ipinfo",
+            connection_name="ipinfo",
+            conf_path=conf_path,
+            config=config,
+        )
---
+++ autobotAI_integrations/integrations/ipstack/__init__.py
@@ -0,0 +1,97 @@
+from typing import List, Type, Union, Optional
+
+from autobotAI_integrations.models import *
+from autobotAI_integrations import (
+    BaseSchema,
+    BaseService,
+    ConnectionInterfaces,
+)
+import requests
+
+
+class IPStackIntegrations(BaseSchema):
+    api_key: Optional[str] = Field(default=None, exclude=True)
+
+    name: Optional[str] = "IPStack"
+    category: Optional[str] = IntegrationCategory.SECURITY_TOOLS.value
+    description: Optional[str] = (
+        "IPStack provides IP to geolocation APIs and global IP database services."
+    )
+
+
+class IPStackService(BaseService):
+
+    def __init__(self, ctx: dict, integration: Union[IPStackIntegrations, dict]):
+        """
+        Integration should have all the data regarding the integration
+        """
+        if not isinstance(integration, IPStackIntegrations):
+            integration = IPStackIntegrations(**integration)
+        super().__init__(ctx, integration)
+
+    def _test_integration(self) -> dict:
+        try:
+            url = f"http://api.ipstack.com/check"
+            response = requests.get(url, params={"access_key": self.integration.api_key})
+
+            if response.status_code == 200:
+                data = response.json()
+                if "success" in data and not data["success"]:
+                    return {"success": False, "error": "Invalid API key"}
+                else:
+                    return {"success": True}
+            else:
+                return {"success": False, "error": f"Request failed with status code: {response.status_code}"}
+        except Exception as e:
+            return {"success": False, "error": f"Error: {str(e)}"}
+
+    @staticmethod
+    def get_forms():
+        return {
+            "label": "IPStack",
+            "type": "form",
+            "children": [
+                {
+                    "name": "api_key",
+                    "type": "text/password",
+                    "label": "API key",
+                    "placeholder": "Enter the IPStack API key",
+                    "required": True,
+                }
+            ],
+        }
+
+    @staticmethod
+    def get_schema() -> Type[BaseSchema]:
+        return IPStackIntegrations
+
+    @classmethod
+    def get_details(cls):
+        return {
+            "clients": [],
+            "supported_executor": "ecs",
+            "compliance_supported": False,
+            "supported_interfaces": cls.supported_connection_interfaces(),
+            "preview": True
+        }
+
+    @staticmethod
+    def supported_connection_interfaces():
+        return [
+            ConnectionInterfaces.STEAMPIPE,
+            ConnectionInterfaces.REST_API,
+        ]
+
+    def generate_steampipe_creds(self) -> SteampipeCreds:
+        conf_path = "~/.steampipe/config/ipstack.spc"
+        config = """connection "ipstack" {
+  plugin = "ipstack"
+}
+"""
+        return SteampipeCreds(
+            envs={"IPSTACK_TOKEN": self.integration.api_key},
+            plugin_name="ipstack",
+            connection_name="ipstack",
+            conf_path=conf_path,
+            config=config,
+        )

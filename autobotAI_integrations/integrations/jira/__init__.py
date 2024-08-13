5a3064e8834824e92d6d010975ecf0abb4a17e66
Ritin Tiwari <ritintiwari417@gmail.com>
2024-08-13 16:14:51 +0530

    added priorities in jira extras

--- autobotAI_integrations/integrations/jira/__init__.py
+++ autobotAI_integrations/integrations/jira/__init__.py
@@ -71,6 +71,7 @@ class JiraService(BaseService):
                 "integration_id": self.integration.accountId,
                 "projects": [project.key for project in jira_cloud.projects()],
                 "issue_types": [issue.name for issue in jira_cloud.issue_types()],
+                "priorities": [priority.name for priority in jira_cloud.priorities()],
             }
         except Exception as e:
             return {"error": "Details can not be fetched"}

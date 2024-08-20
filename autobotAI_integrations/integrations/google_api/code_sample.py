fd04da660f1b1e6869d6f092dea99965174dd14c
Ritin Tiwari <ritintiwari417@gmail.com>
2024-08-20 13:44:15 +0530

    Added python clients and code sample

---
+++ autobotAI_integrations/integrations/google_api/code_sample.py
@@ -0,0 +1,41 @@
+# Import your modules here
+
+# **Security Note:** Client-related modules should not be directly imported here.
+# Instead, they are passed as arguments and retrieved from a secure configuration.
+
+
+def executor(context):
+    """
+    Executes provided Python code within integrations.
+
+    Args:
+        context (dict): A dictionary containing information about the current execution.
+            - params (dict): A dictionary containing parameters specified while creating action.
+            - clients (dict): A dictionary that contain selected client objects while defining action. (The specific clients present and their usage depend on the specific action being executed.)
+
+    Returns:
+        list: Always returns an empty list (`[]`) or a list containing the results of the code execution. The specific content of the returned list depends on the code and how it interacts with the integration.
+    """
+
+    params = context["params"]
+    clients = context["clients"]
+
+    # Placeholder for retrieving the integration-specific client if needed
+    # client = clients["client_name"]  # Uncomment and modify this line to retrieve the client
+
+    # User's Python code execution logic goes here
+    # (Replace this comment with the your actual code)
+
+    # Example: Code to fetch docs (for illustration purposes only)
+    # client = clients["docs"]
+    
+    # The ID of a sample document.
+    # DOCUMENT_ID = "195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE"
+    # try:
+    #     document = client.documents().get(documentId=DOCUMENT_ID).execute()
+
+    #     return document  # Replace with your actual return logic
+    # except BaseException as err:
+    #     return [{
+    #         "error": str(err)
+    #     }]

6dc86a8b0a51bae5bc492e2fb34cb01e3af65a7b
Ritin Tiwari <ritintiwari417@gmail.com>
2024-10-24 16:26:03 +0530

    Enhanced python error response in executor function

--- autobotAI_integrations/utils/__init__.py
+++ autobotAI_integrations/utils/__init__.py
@@ -49,10 +49,17 @@ def load_mod_from_string(code_string):
 
 
 def run_mod_func(fn, **kwargs):
-    if not asyncio.iscoroutinefunction(fn):
-        return fn(**kwargs)
-    else:
-        return asyncio.run(fn(**kwargs))
+    try:
+        if not asyncio.iscoroutinefunction(fn):
+            return fn(**kwargs), None
+        else:
+            return asyncio.run(fn(**kwargs)), None
+    except:
+        try:
+            traceback_execution = traceback.format_exc(chain=True).splitlines()
+            return None, "\n".join(traceback_execution[:1]+ traceback_execution[4: ])
+        except:
+            return None, traceback.format_exc()
 
 
 def list_of_unique_elements(list_to_verify: list) -> list:

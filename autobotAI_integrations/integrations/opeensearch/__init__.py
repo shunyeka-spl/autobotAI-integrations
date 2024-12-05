a0f43b3b6975a046a79a80ca46e51b718b22573e
Ritin Tiwari <ritintiwari417@gmail.com>
2024-12-05 16:07:53 +0530

    Implemented opensearch integration

--- autobotAI_integrations/integrations/opeensearch/__init__.py
+++ autobotAI_integrations/integrations/opeensearch/__init__.py
@@ -1,8 +1,8 @@
-import importlib
+from enum import Enum
 from typing import List, Optional, Type, Union
 
-from pydantic import Field
-import requests
+import boto3
+from pydantic import Field, field_validator
 from autobotAI_integrations import BaseService
 from autobotAI_integrations.models import (
     BaseSchema,
@@ -12,17 +12,57 @@ from autobotAI_integrations.models import (
     SDKCreds,
 )
 from autobotAI_integrations.payload_schema import PayloadTask
+from autobotAI_integrations.utils.boto3_helper import Boto3Helper
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+from utils.logging_config import logger
+
+
+class OpensearchAuthTypes(Enum):
+    AWS_CONFIG = "aws_config"
+    DIRECT_AUTH = "direct_auth"
+
+
+class AWSOpensearchType(Enum):
+    AWS_OPENSEARCH_SERVICE = "aws_opensearch_service"
+    OPENSEARCH_SERVERLESS = "opensearch_serverless"
 
 
 class OpensearchIntegration(BaseSchema):
-    base_url: str = Field(default=None, description="base url", exclude=True)
-    token: Optional[str] = Field(default=None, description="token", exclude=True)
+    host_url: str = Field(default=None, description="base url", exclude=True)
+
+    # On-premise Opensearch
+    port: int = 443
+    username: Optional[str] = Field(default=None, exclude=True)
+    password: Optional[str] = Field(default=None, exclude=True)
+
+    # AWS Opensearch Service
+    region: str
+    opensearch_type: Optional[AWSOpensearchType] = None
+    access_key: Optional[str] = Field(default=None, exclude=True)
+    secret_key: Optional[str] = Field(default=None, exclude=True)
+    session_token: Optional[str] = Field(default=None, exclude=True)
+    account_id: Optional[str] = None
+    roleArn: Optional[str] = None
+    externalId: Optional[str] = None
 
     name: Optional[str] = "Opensearch"
     category: Optional[str] = IntegrationCategory.MONITORING_TOOLS.value
     description: Optional[str] = (
         "OpenSearch is a community-driven, open-source search and analytics suite, based on Elasticsearch and Kibana, designed for scalability and flexibility in search applications."
     )
+
+    def use_dependency(self, dependency: dict):
+        self.roleArn = dependency.get("roleArn")
+        self.access_key = dependency.get("access_key")
+        self.secret_key = dependency.get("secret_key")
+        self.session_token = dependency.get("session_token")
+        self.externalId = dependency.get("externalId")
+        self.account_id = dependency.get("account_id")
+
+    @field_validator("host_url", mode="before")
+    @classmethod
+    def validate_host_url(cls, host_url):
+        return host_url.strip("/")
 
 
 class OpensearchService(BaseService):
@@ -34,41 +74,112 @@ class OpensearchService(BaseService):
             integration = OpensearchIntegration(**integration)
         super().__init__(ctx, integration)
 
+    def _get_aws_credentials(self, aws_client_name: str):
+        if self.integration.roleArn not in ["None", None]:
+            boto3_helper = Boto3Helper(
+                self.ctx, integration=self.integration.dump_all_data()
+            )
+            return boto3_helper.get_session().get_credentials()
+        else:
+            return boto3.Session(
+                aws_access_key_id=self.integration.access_key,
+                aws_secret_access_key=self.integration.secret_key,
+                region_name=self.integration.region,
+                aws_session_token=self.integration.session_token,
+            ).get_credentials()
+
     def _test_integration(self):
         try:
-            headers = {
-                "Authorization": f"ApiKey {self.integration.token}",
-                "Content-Type": "application/json",
-            }
-            # Attempt a health check endpoint
-            response = requests.get(
-                f"{self.integration.base_url.strip('/')}", headers=headers
-            )
-
-            response.raise_for_status()
-            if response.status_code == 200:
-                return {"success": True, "data": response.json()}
+            logger.info(f"Initiating test for integration: {self.integration}")
+            host = self.integration.host_url.split("://")[1]
+            use_ssl = self.integration.host_url.split("://")[0] == "https"
+            client = None
+            if self.integration.auth_type == OpensearchAuthTypes.AWS_CONFIG.value:
+                service = (
+                    "es"
+                    if AWSOpensearchType.AWS_OPENSEARCH_SERVICE.value
+                    == self.integration.opensearch_type
+                    else "aoss"
+                )
+                auth = AWSV4SignerAuth(
+                    self._get_aws_credentials(),
+                    self.integration.region,
+                    service=service,
+                )
+                client = OpenSearch(
+                    hosts=[{"host": host, "port": self.integration.port}],
+                    http_auth=auth,
+                    use_ssl=use_ssl,
+                    verify_certs=use_ssl,
+                    connection_class=RequestsHttpConnection,
+                    pool_maxsize=20,
+                )
+            elif self.integration.auth_type == OpensearchAuthTypes.DIRECT_AUTH.value:
+                auth = (self.integration.username, self.integration.password)
+                client = OpenSearch(
+                    hosts=[{"host": host, "port": self.integration.port}],
+                    http_auth=auth,
+                    use_ssl=use_ssl,
+                    verify_certs=use_ssl,
+                )
             else:
-                return {
-                    "success": False,
-                    "error": f"Request failed with status code: {response.status_code}",
-                }
-        except requests.exceptions.ConnectionError:
-            return {"success": False, "error": "Connection is Unreachable"}
+                return {"success": False, "error": "Invalid Authentication Method."}
+            logger.info(str(client.info()))
         except Exception as e:
-            return {"success": False, "error": str(e)}
+            logger.error(str(e))
+            return {"success": False, "error": "Request failed with unexpected error"}
 
     def build_python_exec_combinations_hook(
         self, payload_task: PayloadTask, client_definitions: List[SDKClient]
     ) -> list:
-        opensearch = importlib.import_module(
-            client_definitions[0].import_library_names[0], package=None
+        logger.info(f"Initiating test for integration: {self.integration}")
+        host = payload_task.creds.envs.get("OPENSEARCH_HOST_URL").split("://")[1]
+        use_ssl = (
+            payload_task.creds.envs.get("OPENSEARCH_HOST_URL").split("://")[0]
+            == "https"
         )
-
-        client = opensearch.Opensearch(
-            payload_task.creds.envs.get("ELASTICSEARCH_API_URL"),
-            api_key=payload_task.creds.envs.get("ELASTICSEARCH_API_KEY"),
-        )
+        client = None
+        if self.integration.auth_type == OpensearchAuthTypes.AWS_CONFIG.value:
+            service = (
+                "es"
+                if AWSOpensearchType.AWS_OPENSEARCH_SERVICE.value
+                == self.integration.opensearch_type
+                else "aoss"
+            )
+            credentials = boto3.Session(
+                aws_access_key_id=payload_task.creds.envs.get("AWS_ACCESS_KEY_ID"),
+                aws_secret_access_key=payload_task.creds.envs.get(
+                    "AWS_SECRET_ACCESS_KEY"
+                ),
+                aws_session_token=payload_task.creds.envs.get("AWS_SESSION_TOKEN"),
+            )
+            auth = AWSV4SignerAuth(
+                credentials, self.integration.region, service=service
+            )
+            client = OpenSearch(
+                hosts=[{"host": host, "port": self.integration.port}],
+                http_auth=auth,
+                use_ssl=use_ssl,
+                verify_certs=use_ssl,
+                connection_class=RequestsHttpConnection,
+                pool_maxsize=20,
+            )
+        elif self.integration.auth_type == OpensearchAuthTypes.DIRECT_AUTH.value:
+            auth = (
+                payload_task.creds.envs.get("OPENSEARCH_USERNAME"),
+                payload_task.creds.envs.get("OPENSEARCH_PASSWORD"),
+            )
+            client = OpenSearch(
+                hosts=[
+                    {
+                        "host": host,
+                        "port": int(payload_task.creds.envs.get("OPENSEARCH_URL_PORT")),
+                    }
+                ],
+                http_auth=auth,
+                use_ssl=use_ssl,
+                verify_certs=use_ssl,
+            )
         return [
             {
                 "clients": {
@@ -86,18 +197,103 @@ class OpensearchService(BaseService):
             "type": "form",
             "children": [
                 {
-                    "name": "base_url",
-                    "type": "text/url",
-                    "label": "API URL",
-                    "placeholder": "API URL",
-                    "required": False,
+                    "label": "AWS Opensearch Integration",
+                    "type": "form",
+                    "formId": OpensearchAuthTypes.AWS_CONFIG.value,
+                    "children": [
+                        {
+                            "name": "host_url",
+                            "type": "text/url",
+                            "label": "Host URL",
+                            "placeholder": "Enter the host URL",
+                            "required": True,
+                        },
+                        {
+                            "name": "integration_id",
+                            "type": "select",
+                            "dataType": "integration",
+                            "label": "Integration Id",
+                            "placeholder": "Enter Integration Id",
+                            "description": "Select Account you want to install this integration",
+                            "required": True,
+                        },
+                        {
+                            "name": "opensearch_type",
+                            "type": "select",
+                            "label": "Select OpenSearch Deployment Type",
+                            "placeholder": "Choose your OpenSearch deployment",
+                            "description": "Please select whether you are using Amazon OpenSearch Service or OpenSearch Serverless for your integration.",
+                            "options": [
+                                {
+                                    "label": "Amazon OpenSearch Service",
+                                    "value": "amazon_opensearch",
+                                },
+                                {
+                                    "label": "OpenSearch Serverless",
+                                    "value": "opensearch_serverless",
+                                },
+                            ],
+                            "required": True,
+                        },
+                        {
+                            "name": "region",
+                            "type": "text",
+                            "label": "Region",
+                            "placeholder": "example: us-east-1",
+                            "required": True,
+                        },
+                    ],
                 },
                 {
-                    "name": "token",
-                    "type": "text/password",
-                    "label": "API Key",
-                    "placeholder": "Your API KEY",
-                    "required": True,
+                    "label": "On-Premises Integration",
+                    "type": "form",
+                    "formId": OpensearchAuthTypes.DIRECT_AUTH.value,
+                    "children": [
+                        {
+                            "name": "host_url",
+                            "type": "text/url",
+                            "label": "Host URL",
+                            "placeholder": "Enter the host URL",
+                            "required": True,
+                        },
+                        {
+                            "name": "port",
+                            "type": "number",
+                            "label": "Port",
+                            "placeholder": "Enter the port number (e.g., 9200)",
+                            "required": True,
+                        },
+                        {
+                            "name": "username",
+                            "type": "text",
+                            "label": "Username",
+                            "placeholder": "Enter your username",
+                            "required": False,
+                        },
+                        {
+                            "name": "password",
+                            "type": "text/password",
+                            "label": "Password",
+                            "placeholder": "Enter your password",
+                            "required": False,
+                        },
+                        {
+                            "name": "use_ssl",
+                            "type": "checkbox",
+                            "label": "Enable SSL/TLS Encryption",
+                            "description": "Enable secure communication using SSL/TLS. Disable only for local or testing environments.",
+                            "default": True,
+                            "required": False,
+                        },
+                        {
+                            "name": "verify_certs",
+                            "type": "checkbox",
+                            "label": "Verify SSL Certificates",
+                            "description": "Ensure the server's SSL certificate is valid. Disable if using self-signed certificates or testing locally.",
+                            "default": True,
+                            "required": False,
+                        },
+                    ],
                 },
             ],
         }
@@ -119,8 +315,21 @@ class OpensearchService(BaseService):
         ]
 
     def generate_python_sdk_creds(self) -> SDKCreds:
-        envs = {
-            "ELASTICSEARCH_API_URL": self.integration.base_url,
-            "ELASTICSEARCH_API_KEY": self.integration.token,
-        }
+        envs = {}
+        if self.integration.auth_type == OpensearchAuthTypes.AWS_CONFIG.value:
+            aws_creds = self._get_aws_credentials()
+            envs = {
+                "OPENSEARCH_HOST_URL": self.integration.host_url,
+                "OPENSEARCH_URL_PORT": 443,
+                "AWS_ACCESS_KEY_ID": aws_creds.access_key,
+                "AWS_SECRET_ACCESS_KEY": aws_creds.secret_key,
+                "AWS_SESSION_TOKEN": aws_creds.session_token,
+            }
+        else:
+            envs = {
+                "OPENSEARCH_HOST_URL": self.integration.host_url,
+                "OPENSEARCH_URL_PORT": self.integration.port,
+                "OPENSEARCH_USERNAME": self.integration.username,
+                "OPENSEARCH_PASSWORD": self.integration.password,
+            }
         return SDKCreds(envs=envs)

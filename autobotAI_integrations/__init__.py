272c1d416a17d7d45032bf53084e80033e584c1e
Ritin Tiwari <ritin.tiwari@shunyeka.com>
2025-01-28 13:06:16 +0530

    Revert "Merge branch 'test' into security-measures"
    This reverts commit d92606857b35b0b0987d38d3aaaf5b712c04dada, reversing
    changes made to 3a74dbd1394aef339b469e17625afef30ebfccb2.

--- autobotAI_integrations/__init__.py
+++ autobotAI_integrations/__init__.py
@@ -31,6 +31,7 @@ from autobotAI_integrations.utils import (
     open_api_parser,
     get_restapi_validated_params,
 )
+from autobotAI_integrations.utils.security_measures import SecurityError
 
 
 class BaseService:

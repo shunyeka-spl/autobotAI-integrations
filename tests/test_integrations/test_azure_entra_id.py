75c86964fbdd7414062c87c3680b7a4fba1b8326
Ritin Tiwari <ritin.tiwari@shunyeka.com>
2025-03-28 15:49:40 +0530

    Fixed errors related to msgraph

--- tests/test_integrations/test_azure_entra_id.py
+++ tests/test_integrations/test_azure_entra_id.py
@@ -1,3 +1,4 @@
+import traceback
 import pytest
 
 from autobotAI_integrations.handlers.task_handler import handle_task
@@ -65,6 +66,41 @@ class TestClassAzure_entra_id:
         result = handle_task(task)
         test_result_format(result)
         print(result.model_dump_json(indent=2))
+
+    def test_actions_generation(self, get_keys):
+        service = integration_service_factory.get_service_cls("azure_entra_id")
+        actions = service.get_all_rest_api_actions()
+        for action in actions:
+            assert action.name is not None
+            assert action.name.strip() != ""
+            print(action.model_dump_json(indent=2))
+        assert len(actions) > 0
+
+    # "Get users"
+    def test_actions_run(
+        self, get_keys, sample_restapi_task, test_result_format, sample_integration_dict
+    ):
+        tokens = {
+            "tenant_id": get_keys["AZURE_TENANT_ID"],
+            "client_id": get_keys["AZURE_CLIENT_ID"],
+            "client_secret": get_keys["AZURE_CLIENT_SECRET"],
+        }
+        integration = sample_integration_dict("azure_entra_id", tokens)
+        service = integration_service_factory.get_service(None, integration)
+        actions = service.get_all_rest_api_actions()
+        for action in actions:
+            if action.name == "Get users":
+                try:
+                    task = sample_restapi_task(
+                        integration, action.code, action.parameters_definition
+                    )
+                    result = handle_task(task)
+                    print(result.model_dump_json(indent=2))
+                    test_result_format(result)
+
+                except Exception as e:
+                    traceback.print_exc()
+                    assert False
 
     def test_integration_active(self, get_keys, sample_integration_dict):
         tokens = {

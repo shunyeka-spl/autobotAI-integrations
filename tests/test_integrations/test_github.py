845a6fecd930493908d812dc2bdd4bf390dfc005
Ritin Tiwari <ritin.tiwari@shunyeka.com>
2025-03-31 15:36:37 +0530

    Added github rest apis

--- tests/test_integrations/test_github.py
+++ tests/test_integrations/test_github.py
@@ -1,3 +1,4 @@
+import traceback
 import pytest
 
 from autobotAI_integrations.handlers.task_handler import handle_task
@@ -58,3 +59,40 @@ class TestClassGithub:
         service = integration_service_factory.get_service(None, integration)
         res = service.is_active()
         assert not res["success"]
+
+    def test_actions_generation(self, get_keys):
+        service = integration_service_factory.get_service_cls("github")
+        actions = service.get_all_rest_api_actions()
+        for action in actions:
+            assert action.name is not None
+            assert action.name.strip() != ""
+            print(action.model_dump_json(indent=2))
+        assert len(actions) > 0
+
+    # "Get a repository"
+    def test_actions_run(
+        self, get_keys, sample_restapi_task, test_result_format, sample_integration_dict
+    ):
+        tokens = {"token": get_keys["GITHUB_TOKEN"]}
+        integration = sample_integration_dict("github", tokens)
+        service = integration_service_factory.get_service(None, integration)
+        actions = service.get_all_rest_api_actions()
+        for action in actions:
+            if action.name == "Get a repository":
+                params = action.parameters_definition
+                for param in params:
+                    if param.name == "owner":
+                        param.values = "<ownervalue>"
+                    if param.name == "repo":
+                        param.values = "<repo-value>"
+                action.parameters_definition = params
+                try:
+                    task = sample_restapi_task(
+                        integration, action.code, action.parameters_definition
+                    )
+                    result = handle_task(task)
+                    print(result.model_dump_json(indent=2))
+                    test_result_format(result)
+                except Exception as e:
+                    traceback.print_exc()
+                    assert False

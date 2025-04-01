7e82a937b59d6667e8f8919fbfae210817b97e96
Tarique-AB <162969628+Tariq-AB@users.noreply.github.com>
2025-04-01 10:26:47 +0530

    handles the scenario where the integration library is in frozen app (#120)
    * Added system interpreter support for Linux integration
    
    * Enforce system Python for LinuxIntegration; raise error if not found
    
    * handle frozen environment

--- autobotAI_integrations/__init__.py
+++ autobotAI_integrations/__init__.py
@@ -1,4 +1,5 @@
 import importlib
+import shutil
 import os
 import subprocess
 import sys
@@ -219,10 +220,22 @@ def executor(context):
     def build_python_exec_combinations(self, payload_task: PayloadTask):
         client_definitions = self.find_client_definitions(payload_task.clients)
         current_installation = set()
+
+        # Check if running in a frozen state (e.g., bundled with PyInstaller for linux integration).
+        if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+            # attempt to locate the system's Python executable.
+            python_exec = shutil.which("python3") or shutil.which("python")
+            exec_mode = "frozen"
+            if not python_exec:
+                raise RuntimeError("Python executable not found on the system.")
+        else:
+            python_exec = sys.executable
+            exec_mode = "source"  
+
         # Installation dir by 'idx' to prevent packages co-interference
         for idx, client in enumerate(client_definitions):
             if client.pip_package_names and not set(client.pip_package_names).issubset(current_installation):
-                logger.info(f"Installing {client.pip_package_names}")
+                logger.info(f"Installing {client.pip_package_names} with python_exec {python_exec} and exec_mode {exec_mode}")
                 try:
                     # prevents the reinstallation of the same package for different tasks
                     subprocess.check_call(
@@ -232,7 +245,7 @@ def executor(context):
                 except subprocess.CalledProcessError:
                     subprocess.check_call(
                         [
-                            sys.executable, "-m",
+                             python_exec, "-m",
                             "pip", "install", " ".join(client.pip_package_names),
                             "-t", f"/tmp/{idx}/", "--no-cache-dir", "--upgrade"
                         ]

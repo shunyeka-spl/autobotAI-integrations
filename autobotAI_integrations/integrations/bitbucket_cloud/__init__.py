be9efe4ba3595d9d5785616bed75ebe7279cd6a4
Ritin Tiwari <ritin.tiwari@shunyeka.com>
2025-05-01 18:26:30 +0530

    Added bitbucket cloud integration

---
+++ autobotAI_integrations/integrations/bitbucket_cloud/__init__.py
@@ -0,0 +1,171 @@
+import base64
+import importlib
+from typing import List, Optional, Union
+
+import requests
+
+from pydantic import Field, field_validator
+
+from autobotAI_integrations import (
+    BaseSchema,
+    SteampipeCreds,
+    RestAPICreds,
+    SDKCreds,
+    BaseService,
+    ConnectionInterfaces,
+    PayloadTask,
+    SDKClient,
+)
+
+from autobotAI_integrations.models import IntegrationCategory
+
+
+class BitBucketCloudIntegration(BaseSchema):
+    base_url: str = Field(default="https://api.bitbucket.org/2.0")
+    username: str = Field(default=None, exclude=True)
+    password: str = Field(default=None, exclude=True)
+
+    name: Optional[str] = "BitBucket Cloud"
+    category: Optional[str] = IntegrationCategory.CODE_REPOSITORY.value
+    description: Optional[str] = "Bitbucket Cloud is a web-based, distributed version control system."
+
+    @classmethod
+    @field_validator("base_url")
+    def validate_base_url(cls, v):
+        if v is None or v.strip() == "":
+            return "https://api.bitbucket.org/2.0"
+        return v.rstrip("/")
+
+
+class BitBucketCloudService(BaseService):
+    def __init__(self, ctx: dict, integration: Union[BitBucketCloudIntegration, dict]):
+        """
+        Integration should have all the data regarding the integration
+        """
+        if not isinstance(integration, BitBucketCloudIntegration):
+            integration = BitBucketCloudIntegration(**integration)
+        super().__init__(ctx, integration)
+
+    def _test_integration(self):
+        # NOTE: o test the integration, we need to get the user read permission
+        user_endpoint = f"{self.integration.base_url}/user"
+        try:
+            response = requests.get(user_endpoint, auth=(self.integration.username, self.integration.password))
+
+            if response.status_code == 200:
+                return {"success": True}
+            elif response.status_code == 401:
+                return {
+                    "success": False,
+                    "error": "Error: Unauthorized. Invalid token or token does not have access to this GitHub instance.",
+                }
+            elif response.status_code == 404:
+                return {
+                    "success": False,
+                    "error": "Error: Not Found. Invalid GitHub URL or endpoint.",
+                }
+            else:
+                return {
+                    "success": False,
+                    "error": f"Error: Unexpected status code {response.status_code}. Response: {response.text}",
+                }
+        except requests.exceptions.RequestException as e:
+            return {"success": False, "error": str(e)}
+
+    @staticmethod
+    def get_forms():
+        return {
+            "label": "BitbucketCloud",
+            "type": "form",
+            "children": [
+                {
+                    "name": "base_url",
+                    "type": "text/url",
+                    "label": "BitbucketCloud Base URL",
+                    "placeholder": "Enter Base URL default: https://api.bitbucketCloud.com",
+                    "description": "BitbucketCloud Base URL if Using Enterprise Version",
+                    "required": False,
+                },
+                {
+                    "name": "username",
+                    "type": "text",
+                    "label": "BitbucketCloud Username",
+                    "placeholder": "Enter your BitbucketCloud username",
+                    "description": "Your BitbucketCloud username for authentication",
+                    "required": True,
+                },
+                {
+                    "name": "password",
+                    "type": "password",
+                    "label": "BitbucketCloud Password or App Password",
+                    "placeholder": "Enter your BitbucketCloud password or app password",
+                    "description": "Your BitbucketCloud password or app password for authentication",
+                    "required": True,
+                }
+            ],
+        }
+
+    @staticmethod
+    def get_schema():
+        return BitBucketCloudIntegration
+    
+    @classmethod
+    def get_details(cls):
+        details = super().get_details()
+        details["preview"] = True
+        return details
+
+    @staticmethod
+    def supported_connection_interfaces():
+        return [
+            ConnectionInterfaces.REST_API,
+            ConnectionInterfaces.PYTHON_SDK,
+        ]
+
+    def build_python_exec_combinations_hook(
+        self, payload_task: PayloadTask, client_definitions: List[SDKClient]
+    ) -> list:
+        bitbucket = importlib.import_module(
+            client_definitions[0].import_library_names[0], package=None
+        )
+        return [
+            {
+                "clients": {
+                    "bitbucket_cloud": bitbucket.Cloud(
+                        username=payload_task.creds.envs.get("BITBUCKET_CLOUD_USERNAME"),
+                        password=payload_task.creds.envs.get("BITBUCKET_CLOUD_PASSWORD"),
+                        cloud=True
+                    )
+                },
+                "params": self.prepare_params(payload_task.params),
+                "context": payload_task.context,
+            }
+        ]
+
+    def generate_steampipe_creds(self) -> SteampipeCreds:
+        pass
+
+    def generate_rest_api_creds(self) -> RestAPICreds:
+        encoded_credentials = base64.b64encode(
+            f"{self.integration.username}:{self.integration.password}".encode(
+                "utf-8"
+            )
+        ).decode("utf-8")
+        return RestAPICreds(
+            base_url=self.integration.base_url.rstrip("/"),
+            headers={
+                "Authorization": f"Basic {encoded_credentials}",
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            verify_ssl=self.integration.base_url.split("://")[0] == "https",
+        )
+
+    def generate_python_sdk_creds(self) -> SDKCreds:
+        return SDKCreds(
+            envs={
+                "BITBUCKET_CLOUD_BASE_URL": self.integration.base_url,
+                "BITBUCKET_CLOUD_USERNAME": self.integration.username,
+                "BITBUCKET_CLOUD_PASSWORD": self.integration.password,
+            }
+        )
---
+++ tests/test_integrations/test_bitbucket_cloud.py
@@ -0,0 +1,98 @@
+import traceback
+import pytest
+
+from autobotAI_integrations.handlers.task_handler import handle_task
+from autobotAI_integrations.integrations import integration_service_factory
+
+bit_bucket_cloud_python_code = """
+def executor(context):
+    bitbucket = context["clients"]["bitbucket_cloud"]
+    results = []
+
+    for workspace in bitbucket.workspaces.each():
+        workspace_data = {}
+        workspace_data["name"] = workspace.name
+        workspace_data["slug"] = workspace.slug
+        workspace_data["uuid"] = workspace.uuid
+        workspace_data["created_on"] = workspace.created_on
+        workspace_data["updated_on"] = workspace.updated_on
+        workspace_data["is_private"] = workspace.is_private
+        results.append(workspace_data)
+
+    return results
+
+"""
+
+
+class TestClassbit_bucket_cloud:
+    def test_bit_bucket_cloud_python_task(
+        self, get_keys, sample_integration_dict, sample_python_task, test_result_format
+    ):
+        tokens = {
+            "username": get_keys["BITBUCKET_CLOUD_USERNAME"],
+            "password": get_keys["BITBUCKET_CLOUD_PASSWORD"],
+        }
+        integration = sample_integration_dict("bitbucket_cloud", tokens)
+        task = sample_python_task(
+            integration, code=bit_bucket_cloud_python_code, clients=["bitbucket_cloud"]
+        )
+        result = handle_task(task)
+        test_result_format(result)
+
+    def test_integration_active(self, get_keys, sample_integration_dict):
+        tokens = {
+            "username": get_keys["BITBUCKET_CLOUD_USERNAME"],
+            "password": get_keys["BITBUCKET_CLOUD_PASSWORD"],
+        }
+        integration = sample_integration_dict("bitbucket_cloud", tokens)
+        service = integration_service_factory.get_service(None, integration)
+        res = service.is_active()
+        assert res["success"]
+        tokens = {
+            "username": get_keys["BITBUCKET_CLOUD_USERNAME"],
+            "password": get_keys["BITBUCKET_CLOUD_PASSWORD"][:-2],
+        }
+        integration = sample_integration_dict("bitbucket_cloud", tokens)
+        service = integration_service_factory.get_service(None, integration)
+        res = service.is_active()
+        assert not res["success"]
+
+    def test_actions_generation(self, get_keys):
+        service = integration_service_factory.get_service_cls("bitbucket_cloud")
+        actions = service.get_all_rest_api_actions()
+        for action in actions:
+            assert action.name is not None
+            assert action.name.strip() != ""
+            print(action.model_dump_json(indent=2))
+        assert len(actions) > 0
+
+    # "Get a repository"
+    def test_actions_run(
+        self, get_keys, sample_restapi_task, test_result_format, sample_integration_dict
+    ):
+        tokens = {
+            "username": get_keys["BITBUCKET_CLOUD_USERNAME"],
+            "password": get_keys["BITBUCKET_CLOUD_PASSWORD"],
+        }
+        integration = sample_integration_dict("bitbucket_cloud", tokens)
+        service = integration_service_factory.get_service(None, integration)
+        actions = service.get_all_rest_api_actions()
+        for action in actions:
+            if action.name == "Get a repository":
+                params = action.parameters_definition
+                for param in params:
+                    if param.name == "repo_slug":
+                        param.values = "test-repo"
+                    if param.name == "workspace":
+                        param.values = "test-workspace-0204"
+                action.parameters_definition = params
+                try:
+                    task = sample_restapi_task(
+                        integration, action.code, action.parameters_definition
+                    )
+                    result = handle_task(task)
+                    print(result.model_dump_json(indent=2))
+                    test_result_format(result)
+                except Exception as e:
+                    traceback.print_exc()
+                    assert False

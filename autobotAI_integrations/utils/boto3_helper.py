a218c4a72f43a1aa4e42f438becf99be9f691dd1
Vishal Shandilya <vishal.shandilya@shunyeka.com>
2025-07-08 13:24:32 +0530

    Added Better Logging of Access Key

--- autobotAI_integrations/utils/boto3_helper.py
+++ autobotAI_integrations/utils/boto3_helper.py
@@ -149,7 +149,7 @@ class Boto3Helper:
                     assumerole = sts_client.get_session_token()
                 self.credentials = assumerole['Credentials']
                 self.credentials["stsCredsGeneratedOn"] = datetime.now().isoformat()
-                self.ctx.logger.info(f"Using Access Key with ID {self.credentials['AccessKeyId']}")
+                self.ctx.logger.info(f" [Integrations] Using Access Key with ID: {self.credentials['AccessKeyId']}, Session Token: {self.credentials['SessionToken']} and Expiration: {self.credentials['Expiration']}")
             except ClientError as e:
                 if "ThrottlingException" in str(e):
                     sleep(3)

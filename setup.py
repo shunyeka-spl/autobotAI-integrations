b54f431c5155412ad418fd58b2942e1edcd30258
Vishal Shandilya <vishal.shandilya@shunyeka.com>
2025-09-11 18:01:39 +0530

    Adds JWT Requirement

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
         "PyYAML>=6.0.2",
         "pydash>=8.0.5",
         "requests>=2.32.4",
-        "tenacity>=8.5.0"
+        "tenacity>=8.5.0",
+        "jwt>=1.4.0"
     ],
     extras_require={
         "executable": [

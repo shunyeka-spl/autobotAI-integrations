f399feaef9ba91cc1957e45f63caaffc5e54b310
Abhishek-autobotai <abhishek.rathod@autobot.live>
2026-08-14 12:29:25 +0530

    fix: add helper function of mcp ssl

--- autobotAI_integrations/integrations/gitlab/__init__.py
+++ autobotAI_integrations/integrations/gitlab/__init__.py
@@ -223,5 +223,4 @@ class GitlabService(BaseService):
             headers={
                 "Authorization": f"Bearer {self.integration.token}",
             },
-            ignore_ssl=bool(self.integration.ignore_ssl),
         )
